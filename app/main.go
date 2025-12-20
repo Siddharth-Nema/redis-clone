bdb70403c14a962f73c92e314aa613dfd1dbceb0
Siddharth-Nema <siddharth.nema6362@gmail.com>
2025-12-20 13:23:48 +0000

    fix: may work

--- app/main.go
+++ app/main.go
@@ -18,14 +18,12 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-		}
-		go handleConnection(conn)
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
 	}
+	handleConnection(conn)
 }
 
 func handleConnection(conn net.Conn) {
